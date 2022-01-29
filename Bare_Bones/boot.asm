39865dd2b2bbbb7663d201f6a1661ea8b920560c
Ftor7 <vfndtq200433@yandex.ru>
2022-01-29 20:44:04 +0300

    init boot.asm

--- Bare_Bones/boot.asm
+++ Bare_Bones/boot.asm
@@ -1,1 +1,29 @@
+MAGIC       equ    0x1BAD8002
+FLAGS       equ    0
+CHECKSUM   equ    -(MAGIC + FLAGS)
 
+
+	section .multiboot
+dd MAGIC
+dd FLAGS
+dd CHECKSUM
+
+
+	section .bss
+align 16
+stack_bottom:
+resb 16384
+stack_top:
+
+
+	section .text
+global _start
+_start:
+	mov eax, stack_top
+
+	extern kernel_main
+	call kernel_main
+
+	cli
+.halt:	hlt
+	jmp .halt
